976ea5c6ac90d9c7c18ddb70a62cec2724b637fe
Carlos E. Salazar <ce.salazar@gmail.com>
2026-02-23 02:36:46 +0100

    Update plugins

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+
+  -- Make TokyoNight Transparent
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     -- transparent = true,
+  --     -- styles = {
+  --     --   sidebars = "transparent",
+  --     --   floats = "transparent",
+  --     -- },
+  --   },
+  -- },
+}
