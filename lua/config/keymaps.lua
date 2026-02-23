64f6086931099a688da2c67b78797ec10c7e91da
Carlos E. Salazar <ce.salazar@gmail.com>
2026-02-23 03:09:53 +0100

    Update options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,13 +12,17 @@ map("n", "<M-o>", ":BufferLineCycleNext<cr>", { desc = "Go to next Buffer", rema
 map("n", "<M-x>", ":bd<cr>", { desc = "Delete Buffer", remap = true, silent = true })
 
 map("n", "Y", "yy", { desc = "Copy line with linebreak", remap = true, silent = true })
-map("n", "\\w", ":w<cr>", { desc = "Write Buffer", remap = true, silent = true }) -- TODO: Odd behaviour
+map("n", "\\\\", "<cmd>w<cr>", { desc = "Write Buffer", silent = true })
 
-map("n", "\\l", ":set list!<cr>", { desc = "Toggle List Mode", remap = true, silent = true })
-map("n", "\\t0", ":set textwidth=0 colorcolumn=<cr>", { desc = "Reset textwidth", remap = true, silent = true })
-map("n", "\\t5", ":set textwidth=50 colorcolumn=+1<cr>", { desc = "Set textwidth=50", remap = true, silent = true })
-map("n", "\\t7", ":set textwidth=72 colorcolumn=+1<cr>", { desc = "Set textwidth=72", remap = true, silent = true })
-map("n", "\\t8", ":set textwidth=80 colorcolumn=+1<cr>", { desc = "Set textwidth=80", remap = true, silent = true })
+Snacks.toggle.option("list", { name = "List Mode" }):map("\\l")
+Snacks.toggle.option("wrap", { name = "Wrap" }):map("\\w")
+
+map("n", "\\t0", "<cmd>set textwidth=0 colorcolumn=<cr>", { desc = "Reset textwidth", silent = true })
+map("n", "\\t5", "<cmd>set textwidth=50 colorcolumn=+1<cr>", { desc = "Set textwidth=50", silent = true })
+map("n", "\\t7", "<cmd>set textwidth=72 colorcolumn=+1<cr>", { desc = "Set textwidth=72", silent = true })
+map("n", "\\t8", "<cmd>set textwidth=80 colorcolumn=+1<cr>", { desc = "Set textwidth=80", silent = true })
+
+map("n", "<M-n>", "<leader>fe", { desc = "Explorer Snacks (root dir)", remap = true })
 
 map("v", "<M-s>", ":sort<cr>", { desc = "Sort selection", remap = true, silent = true })
-map("v", "<M-y>", '"+y', { desc = "Yank to clipboard", remap = true, silent = true })
+map("v", "<M-y>", '"+y', { desc = "Yank to clipboard", remap = true })
