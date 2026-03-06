94e4d5a20f33ceff2d3b42bee1bd099d843c6660
Carlos E. Salazar <ce.salazar@gmail.com>
2026-03-06 23:46:56 +0100

    Update maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,27 +6,29 @@
 local map = vim.keymap.set
 
 -- ── Buffers ──────────────────────────────────────────────────────────────
-map("n", ",", "<C-^>", { desc = "Return to last Buffer", remap = true, silent = true })
-map("n", "<M-i>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer", remap = true, silent = true })
-map("n", "<M-o>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer", remap = true, silent = true })
-map("n", "<M-x>", "<cmd>bd<cr>", { desc = "Delete Buffer", remap = true, silent = true })
-map("n", "<M-n>", "<leader>fe", { desc = "Explorer Snacks (root dir)", remap = true })
+map("n", ",", "<C-^>", { desc = "Switch to Other Buffer", remap = true, silent = true })
+map("n", "<leader>bn", "<Cmd>bn<CR>", { desc = "Next Buffer", remap = true })
+map("n", "<leader>bp", "<Cmd>bp<CR>", { desc = "Prev Buffer", remap = true })
+map("n", "<M-f>", "<leader><space>", { desc = "Find Files (Root Dir)", remap = true })
+map("n", "<M-x>", "<Cmd>bd<CR>", { desc = "Delete Buffer", remap = true, silent = true })
 
 -- ── File ─────────────────────────────────────────────────────────────────
-map("n", "\\\\", "<cmd>w<cr>", { desc = "Write Buffer", silent = true })
+map("n", "\\\\", "<Cmd>w<CR>", { desc = "Write Buffer", silent = true })
+map("n", "<M-n>", "<leader>fE", { desc = "Explorer Snacks (cwd)", remap = true })
+map("n", "<M-N>", "<leader>fe", { desc = "Explorer Snacks (Root Dir)", remap = true })
 
 -- ── Editing ──────────────────────────────────────────────────────────────
 map("n", "Y", "yy", { desc = "Copy line with linebreak", remap = true, silent = true })
-map("v", "<M-s>", ":sort<cr>", { desc = "Sort selection", remap = true, silent = true })
+map("v", "<M-s>", ":sort l<CR>", { desc = "Sort selection", remap = true, silent = true })
 map("v", "<M-y>", '"+y', { desc = "Yank to clipboard", remap = true })
 
 -- ── Options ──────────────────────────────────────────────────────────────
 Snacks.toggle.option("list", { name = "List Mode" }):map("\\l")
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("\\w")
 
-map("n", "\\t0", "<cmd>set textwidth=0 colorcolumn=<cr>", { desc = "Reset textwidth", silent = true })
-map("n", "\\t2", "<cmd>set textwidth=120 colorcolumn=+1<cr>", { desc = "Set textwidth=120", silent = true })
-map("n", "\\t5", "<cmd>set textwidth=50 colorcolumn=+1<cr>", { desc = "Set textwidth=50", silent = true })
-map("n", "\\t7", "<cmd>set textwidth=72 colorcolumn=+1<cr>", { desc = "Set textwidth=72", silent = true })
-map("n", "\\t8", "<cmd>set textwidth=80 colorcolumn=+1<cr>", { desc = "Set textwidth=80", silent = true })
-map("n", "\\tg", "<cmd>GitCommitLimits<cr>", { desc = "Set commit textwidth", silent = true })
+map("n", "\\t0", "<Cmd>set textwidth=0 colorcolumn=<CR>", { desc = "Reset textwidth", silent = true })
+map("n", "\\t2", "<Cmd>set textwidth=120 colorcolumn=+1<CR>", { desc = "Set textwidth=120", silent = true })
+map("n", "\\t5", "<Cmd>set textwidth=50 colorcolumn=+1<CR>", { desc = "Set textwidth=50", silent = true })
+map("n", "\\t7", "<Cmd>set textwidth=72 colorcolumn=+1<CR>", { desc = "Set textwidth=72", silent = true })
+map("n", "\\t8", "<Cmd>set textwidth=80 colorcolumn=+1<CR>", { desc = "Set textwidth=80", silent = true })
+map("n", "\\tg", "<Cmd>GitCommitLimits<CR>", { desc = "Set commit textwidth", silent = true })
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,13 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    keys = {
+      { "]b", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+      { "[b", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+      { "<leader>bi", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<M-i>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+      { "<M-o>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+    },
     opts = {
       options = {
         always_show_bufferline = true,
