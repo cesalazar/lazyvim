291e396c2a9800165652124517def538412f96eb
Carlos E. Salazar <ce.salazar@gmail.com>
2026-03-13 19:23:20 +0100

    Set black as the python formatter

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,7 +1,11 @@
 return {
+  -- Add themes here
+  -- { "samharju/synthweave.nvim" },
+
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "synthweave",
       colorscheme = "tokyonight-night",
     },
   },
