97c1a55c6debbe77d5e37ea219d70712d647ca44
Carlos E. Salazar <ce.salazar@gmail.com>
2026-03-26 12:16:17 +0100

    Update logic to disable diagnostics by code

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,11 +6,14 @@ return {
       servers = {
         vtsls = {
           handlers = {
-            -- Disable diagnostic "Could not find a declaration file for module ..."
+            -- Disabled diagnostics:
+            -- 6133 - Could not find a declaration file for module...
+            -- 7016 - The imports and exports are not sorted
             ["textDocument/publishDiagnostics"] = function(err, result, ctx)
+              local ignoredCodes = { 6133, 7016 }
               if result and result.diagnostics then
                 result.diagnostics = vim.tbl_filter(function(d)
-                  return d.code ~= 7016
+                  return not vim.tbl_contains(ignoredCodes, d.code)
                 end, result.diagnostics)
               end
               vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
